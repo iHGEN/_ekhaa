70a2d43ce7ecc19a2a4f204bf8b12f9074f36fab
Hatem <64999740+iHGEN@users.noreply.github.com>
2023-12-16 02:06:11 +0300

    add library & coffe

---
+++ Assets/SaudiModel/Scripts/AnimationController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator MainAnimator;
+    public GameObject NormalCup;
+    public GameObject NormalDallah;
+
+    public GameObject AnimationCup;
+    public GameObject AnimationDallah;
+
+    public AudioClip[] audioClips; // Array of audio clips
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGreetingSound()
+    {
+            audioSource.clip = audioClips[2];
+            audioSource.Play();
+    }
+
+    public void PlayCoffeeSound()
+    {
+            audioSource.clip = audioClips[0];
+            audioSource.Play();
+    }
+
+    public void PlayDrinkingSound()
+    {
+            audioSource.clip = audioClips[1];
+            audioSource.Play();
+
+    }
+    public void RotateCup()
+    {
+        MainAnimator.Play("RotateCup",1);
+
+    }
+
+    public void RotateCupBack()
+    {
+        MainAnimator.Play("RotateCupBack",1);
+
+    }
+
+    public void HideAnimationCupAndDisplayNormalCup()
+    {
+        NormalCup.SetActive(true);
+        AnimationCup.SetActive(false);
+
+    }
+
+    public void HideNormalDallahANDCUPAndDisplayAnimationDallahANDCup()
+    {
+        NormalDallah.SetActive(false);
+        NormalCup.SetActive(false);
+        AnimationDallah.SetActive(true);
+        AnimationCup.SetActive(true);
+        
+    }
+
+    public void HideAnimationDallahAndDisplayNormalDallah()
+    {
+
+        NormalDallah.SetActive(true);
+        AnimationDallah.SetActive(false);
+    }
+}
--- Assets/script/AI_inraction.cs
+++ Assets/script/AI_inraction.cs
@@ -14,8 +14,6 @@ public class AI_inraction : MonoBehaviour
     [SerializeField] AudioClip[] _audio;
     [SerializeField] GameObject[] charcters_prefab;
     [SerializeField] GameObject[] waypoint;
-    [SerializeField] Quaternion[] rot_waypoint;
-    [SerializeField] GameObject[] stagepoint;
     [SerializeField] float charcters_speed;
     [SerializeField] float charcters_hight;
     [SerializeField] float charcters_radius;
@@ -28,27 +26,18 @@ public class AI_inraction : MonoBehaviour
     AudioSource[] audioSources;
     [SerializeField] float distance;
     [SerializeField] GameObject start_location;
-    [field: SerializeField] Quaternion stage_rot { get; set; }
     float[] timer;
-    bool[] stage_audio_play;
     bool[] _is_location_reached;
     int[] index;
-    public bool go_to_stage;
-    bool[] is_going_to_stage;
-    int[] _point_number;
-    public bool is_clapping;
-    bool add_timer = true;
     float _audio_timer;
     bool[] _is_spaown_done;
     readonly int Walk = Animator.StringToHash("Walk");
-    readonly int Clapping = Animator.StringToHash("Clapping");
+    readonly int Pulling = Animator.StringToHash("pulling");
+    readonly int Dwarf = Animator.StringToHash("dwarf");
     void Start()
     {
         _is_spaown_done = new bool[charcters_prefab.Length];
-        stage_audio_play = new bool[charcters_prefab.Length];
         audioSources = new AudioSource[charcters_prefab.Length];
-        _point_number = new int[charcters_prefab.Length];
-        is_going_to_stage = new bool[charcters_prefab.Length];
         _is_location_reached = new bool[charcters_prefab.Length];
         charcter_Animator = new Animator[charcters_prefab.Length];
         navMeshAgents = new NavMeshAgent[charcters_prefab.Length];
@@ -82,94 +71,7 @@ public class AI_inraction : MonoBehaviour
             yield return new WaitForSeconds(Respaown_delay);
         }
     }
-    void prevent_charcter()
-    {
-        for (int x = 0; x < charcters_prefab.Length; x++)
-        {
-            charcter_Animator[x].SetBool(Clapping, is_clapping);
-            is_going_to_stage[x] = false;
-            stage_audio_play[x] = false;
-            _point_number[x] = 0;
-        }
-    }
-    void pick_random_charcter_to_go_stage()
-    {
-        int number = 0;
-        add_timer = false;
-        prevent_charcter();
-        for (int i = 0; i < 7; i++)
-        {
-        il:
-            number = Random.Range(0, charcters_prefab.Length);
-            if (is_going_to_stage[number])
-            {
-                goto il;
-            }
-            _point_number[number] = i;
-            timer[number] = 5f;
-            stage_audio_play[number] = true;
-            is_going_to_stage[number] = true;
-        }
-    }
-    void walk_to_stage()
-    {
-        if(!go_to_stage)
-        {
-            return;
-        }
-        if (add_timer)
-        {
-            pick_random_charcter_to_go_stage();
-        }
-        for (int i = 0; i < charcters_prefab.Length; i++)
-        {
-            if (charcters_prefab[i] != null && is_going_to_stage[i] && _is_spaown_done[i])
-            {
-                var dis = Vector3.Distance(charcters_prefab[i].transform.position, stagepoint[_point_number[i]].transform.position);
-                apply_animator_controller(charcter_Animator[i]);
-                charcter_Animator[i].SetBool(Walk, dis > 0.65f);
-                navMeshAgents[i].transform.rotation = (dis < 0.65f) ? Quaternion.Lerp(navMeshAgents[i].transform.rotation, stage_rot, 0.1f) : navMeshAgents[i].transform.rotation;
-                charcter_Animator[i].SetBool(Clapping, is_clapping && dis < 0.65f);
-                if(is_clapping && dis < 0.65f)
-                {
-                    if(stage_audio_play[i])
-                    {
-                        stage_audio_play[i] = false;
-                        audioSources[i].PlayOneShot(_audio[0], 1f);
-                    }
-                }
-                if (Vector3.Distance(charcters_prefab[i].transform.position, _player.transform.position) < _player_distance && _audio_timer <= 0 && !is_clapping && dis > 0.65f) 
-                {
-                    _audio_timer = time_to_next_intract_audio;
-                    if (get_audio_for_intract_for_charcter(charcters_prefab[i]) != null)
-                    {
-                        audioSources[i].PlayOneShot(get_audio_for_intract_for_charcter(charcters_prefab[i]), 0.7f);
-                    }
-                }
-                if (is_clapping)
-                {
-                    timer[i] -= Time.deltaTime;
-                    if (timer[i] <= 0)
-                    {
-                        is_clapping = false;
-                        go_to_stage = false;
-                        add_timer = true;
-                        prevent_charcter();
-                        timer[i] = charcters_stop_time;
-                    }
-                }
-                else
-                {
-                    navMeshAgents[i].SetDestination(stagepoint[_point_number[i]].transform.position);
-                }
-                var lockd = Quaternion.LookRotation(new Vector3(navMeshAgents[i].destination.x, 0, navMeshAgents[i].destination.z));
-                if (lockd.eulerAngles.x != 0 || lockd.eulerAngles.z != 0)
-                {
-                    navMeshAgents[i].transform.Rotate(0, lockd.eulerAngles.normalized.magnitude, 0);
-                }
-            }
-        }
-    }
+    
     void ai_all_location_reached(bool _is_reset)
     {
         for(int i = 0; i < charcters_prefab.Length;i++)
@@ -186,7 +88,7 @@ public class AI_inraction : MonoBehaviour
     }
     AudioClip get_audio_for_intract_for_charcter(GameObject charcter)
     {
-        int num = Random.Range(!charcter.name.Contains("Male") ? 1 : 4, charcter.name.Contains("Male") ? 4 : 8);
+        int num = Random.Range(0,_audio.Length);
         if(num > _audio.Length - 1)
         {
             return null;
@@ -197,16 +99,16 @@ public class AI_inraction : MonoBehaviour
     {
         for (int i = 0; i < charcters_prefab.Length; i++)
         {
-            if (charcters_prefab[i] != null && !is_going_to_stage[i]&& _is_spaown_done[i])
+            if (charcters_prefab[i] != null && _is_spaown_done[i])
             {
                 var dis = Vector3.Distance(charcters_prefab[i].transform.position, waypoint[index[i]].transform.position);
                 apply_animator_controller(charcter_Animator[i]);
-                //if (_is_location_reached[i])
-                //{
-                //    index[i] = Random.Range(0,(_Design._design_point) ? 14 : waypoint.Length);
-                //    _is_location_reached[i] = false;
-                //}
-                if (Vector3.Distance(charcters_prefab[i].transform.position, _player.transform.position) < _player_distance && _audio_timer <= 0 && !is_clapping && dis > 0.65f)
+                if (_is_location_reached[i])
+                {
+                    index[i] = Random.Range(0,waypoint.Length);
+                    _is_location_reached[i] = false;
+                }
+                if (Vector3.Distance(charcters_prefab[i].transform.position, _player.transform.position) < _player_distance && _audio_timer <= 0)
                 {
                     _audio_timer = time_to_next_intract_audio;
                     if (get_audio_for_intract_for_charcter(charcters_prefab[i]) != null)
@@ -214,14 +116,9 @@ public class AI_inraction : MonoBehaviour
                         audioSources[i].PlayOneShot(get_audio_for_intract_for_charcter(charcters_prefab[i]), 1f);
                     }
                 }
-                //if (_Design._reset_ai_position)
-                //{
-                //    ai_all_location_reached(_Design._reset_ai_position);
-                //    _Design._reset_ai_position = false;
-                //}
                 charcter_Animator[i].SetBool(Walk, dis > _action_distance);
-              //  charcter_Animator[i].SetBool(Clapping, is_clapping);
-                navMeshAgents[i].transform.rotation = (dis < _action_distance) ? Quaternion.Lerp(navMeshAgents[i].transform.rotation, rot_waypoint[index[i]], 0.1f) : navMeshAgents[i].transform.rotation;
+                charcter_Animator[i].SetBool(Pulling, dis < _action_distance);
+                navMeshAgents[i].transform.rotation = (dis < _action_distance) ? Quaternion.Lerp(navMeshAgents[i].transform.rotation, waypoint[index[i]].transform.rotation, 0.2f) : navMeshAgents[i].transform.rotation;
                 if (dis < distance)
                 {
                     timer[i] -= Time.deltaTime;
@@ -251,7 +148,6 @@ public class AI_inraction : MonoBehaviour
     void Update()
     {
         Audio_timer();
-        walk_to_stage();
         walk_random();
     }
 }
